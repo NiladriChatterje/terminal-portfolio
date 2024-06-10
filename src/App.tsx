5534b6a8dde181a2e422875ac4a7fe6da3aea696
NiladriChatterje <radongtxgpu@gmail.com>
2024-06-10 15:14:50 +0530

    fitAddon is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Terminal, ITerminalOptions } from "@xterm/xterm"
+import { useEffect, useRef } from "react"
+import styles from './App.module.css'
+
+const TerminalStyle = {
+  width: '100%',
+  height: '100%',
+  position: 'fixed',
+  top: 0, left: 0
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const terminalRef = useRef<HTMLDivElement | any>(null);
+
+  useEffect(() => {
+    const terminal = new Terminal({
+      allowTransparency: true,
+      cursorInactiveStyle: 'outline',
+      cursorBlink: true,
+      cursorWidth: 10,
+      cursorStyle: 'underline',
+      rows: 40, cols: 170
+    });
+    terminal.open(terminalRef.current);
+    terminal.write('welcome/{user}/$ ');
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section>
+      <div id={styles.terminal} ref={terminalRef} />
+    </section>
   )
 }
 
