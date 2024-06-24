b7684629540381acf09d9fe730ca6c07112bdb74
NiladriChatterje <radongtxgpu@gmail.com>
2024-06-24 22:19:50 +0530

    key event issue

--- src/components/Commands.ts
+++ src/components/Commands.ts
@@ -1,4 +1,4 @@
-import { terminal } from "./Terminal";
+import { terminal } from './Terminal';
 import {
     bsc_final, fetchGithubRepos, redirectToRepo, showSkills,
     certificates, github, hs_result, icse_result, linkedIn, mca_final,
@@ -27,12 +27,12 @@ export function clearUpToRow() {
 
     const renderText = createArrowText();
     terminal.write(renderText)
-    terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m ");
+    terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `);
 }
 
 function showMCAResult() {
     terminal.writeln(mca_final);
-    terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m ");
+    terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `);
 }
 
 
@@ -40,35 +40,35 @@ function showMCAResult() {
 const ExistingCommand = new Map<string | RegExp, any>(
     [
         ['man', `\n
-  ${"ls skill".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"list all the skills acquired"}
-  ${"ls cf".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"list all but only certificates"}
-  ${"cls".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"clear the console."}
-  ${"show mca".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"Display MCA result."}
-  ${"show bsc".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"Display BSc(H) result."}
-  ${"show hs".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"display Higher Secondary result"}
-  ${"show icse".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"display Secondary result"}
-  ${"fetch github".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"provide github link"}
-  ${"fetch linkedin".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"provide linkedin link"}
-  ${"fetch x".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"provide twitter(X) link"}
-  ${"github ls".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"list some of my top projects"}
-  ${"github show <repo>".padEnd(window.innerWidth > 1200 ? 35 : 20)}${"redirect to the repository"}\n`],
-        ["cls", clearUpToRow],
-        ["show mca", showMCAResult],
-        ["show bsc", () => { terminal.writeln(bsc_final); terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m "); }],
-        ["show hs", () => { terminal.writeln(hs_result); terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m "); }],
-        ["show icse", () => { terminal.writeln(icse_result); terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m "); }],
-        ["ls cf", () => { terminal.writeln(certificates); terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m "); }],
-        ["fetch linkedin", () => { terminal.writeln(linkedIn); terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m "); }],
-        ["fetch github", () => { terminal.writeln(github); terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m "); }],
-        ["fetch x", () => { terminal.writeln(twitter); terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m "); }],
-        ["github ls", fetchGithubRepos],
-        ["ls skill", showSkills]
+  ${`ls skill`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`list all the skills acquired`}
+  ${`ls cf`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`list all but only certificates`}
+  ${`cls`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`clear the console.`}
+  ${`show mca`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`Display MCA result.`}
+  ${`show bsc`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`Display BSc(H) result.`}
+  ${`show hs`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`display Higher Secondary result`}
+  ${`show icse`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`display Secondary result`}
+  ${`fetch github`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`provide github link`}
+  ${`fetch linkedin`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`provide linkedin link`}
+  ${`fetch x`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`provide twitter(X) link`}
+  ${`github ls`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`list some of my top projects`}
+  ${`github show <repo>`.padEnd(window.innerWidth > 1200 ? 35 : 20)}${`redirect to the repository`}\n`],
+        [`cls`, clearUpToRow],
+        [`show mca`, showMCAResult],
+        [`show bsc`, () => { terminal.writeln(bsc_final); terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `); }],
+        [`show hs`, () => { terminal.writeln(hs_result); terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `); }],
+        [`show icse`, () => { terminal.writeln(icse_result); terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `); }],
+        [`ls cf`, () => { terminal.writeln(certificates); terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `); }],
+        [`fetch linkedin`, () => { terminal.writeln(linkedIn); terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `); }],
+        [`fetch github`, () => { terminal.writeln(github); terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `); }],
+        [`fetch x`, () => { terminal.writeln(twitter); terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m `); }],
+        [`github ls`, fetchGithubRepos],
+        [`ls skill`, showSkills]
     ]
 );
 
 export const handleCommand = (command: string) => {
-    if (command.includes("github show")) {
-        const tokenization = command.split(" ");
+    if (command.includes(`github show`)) {
+        const tokenization = command.split(` `);
         if (tokenization.length < 3) throw new Error();
 
         const project: string | any = tokenization.pop();
@@ -82,7 +82,7 @@ export const handleCommand = (command: string) => {
     else {
         terminal.write('\x1b[J');
         terminal.writeln(ExistingCommand.get('man'));
-        terminal.write("\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m ");
+        terminal.write(`\n\x1b[103m \x1b[30m$command/here $ ↵ \x1b[0m\x1b[93m▶\x1b[0m  `);
     }
 
 }
